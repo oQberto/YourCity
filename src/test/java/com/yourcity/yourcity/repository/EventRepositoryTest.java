c2cf5d42ec11383e072f76a43622ddf94ce5a829
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2024-03-09 18:45:40 +0300

    feat-test: create unit tests for EventRepository.java

---
+++ src/test/java/com/yourcity/yourcity/repository/EventRepositoryTest.java
@@ -0,0 +1,98 @@
+package com.yourcity.yourcity.repository;
+
+import com.yourcity.yourcity.TestYourCityApplication;
+import com.yourcity.yourcity.model.entity.Event;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Optional;
+
+import static lombok.AccessLevel.PRIVATE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+
+
+@RequiredArgsConstructor
+@Sql({
+        "classpath:sql/test-data-clear.sql",
+        "classpath:sql/test-data-insert.sql"
+})
+@FieldDefaults(makeFinal = true, level = PRIVATE)
+@SpringBootTest(classes = TestYourCityApplication.class)
+public class EventRepositoryTest {
+    EventRepository eventRepository;
+
+    @Test
+    public void findByName_whenNameExists_shouldReturnEvent() {
+        var name = "Event1";
+
+        Optional<Event> actualUser = eventRepository.findByName(name);
+
+        assertThat(actualUser).isPresent();
+        assertThat(actualUser.get().getId()).isEqualTo(1L);
+    }
+
+    @Test
+    public void findByName_whenNameDoesNotExists_shouldReturnEmptyOptional() {
+        var notExistedName = anyString();
+
+        Optional<Event> actualUser = eventRepository.findByName(notExistedName);
+
+        assertThat(actualUser).isEmpty();
+    }
+
+    @Test
+    public void findAllByEventTime_whenEventTimeExists_shouldReturnListOfEvents() {
+        var existingEventTime = LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES).plusDays(1L);
+        var pageableWithFiveElements = PageRequest.of(0, 5);
+        var pageableWithHundredElements = PageRequest.of(0, 100);
+
+        Page<Event> actualFiveEvents = eventRepository.findAllByEventTime(existingEventTime, pageableWithFiveElements);
+        Page<Event> actualHundredEvents = eventRepository.findAllByEventTime(existingEventTime, pageableWithHundredElements);
+
+        assertThat(actualFiveEvents).hasSize(5);
+        assertThat(actualHundredEvents).hasSize(30);
+    }
+
+    @Test
+    public void findAllByEventTime_whenEventTimeDoesNotExist_shouldReturnEmptyList() {
+        var notExistedEventTime = any(LocalDateTime.class);
+        var anyPageable = any(Pageable.class);
+
+        Page<Event> actualEvents = eventRepository.findAllByEventTime(notExistedEventTime, anyPageable);
+
+        assertThat(actualEvents).isEmpty();
+    }
+
+    @Test
+    public void findAllByOwnerUsername_whenOwnerExists_shouldReturnListOfEvents() {
+        var ownerName = "user1";
+        var pageableWithFiveElements = PageRequest.of(0, 5);
+        var pageableWithHundredElements = PageRequest.of(0, 100);
+
+        Page<Event> actualFiveEvents = eventRepository.findAllByOwnerUsername(ownerName, pageableWithFiveElements);
+        Page<Event> actualHundredEvents = eventRepository.findAllByOwnerUsername(ownerName, pageableWithHundredElements);
+
+        assertThat(actualFiveEvents).hasSize(5);
+        assertThat(actualHundredEvents).hasSize(30);
+    }
+
+    @Test
+    public void findAllByOwnerUsername_whenOwnerDoesNotExist_shouldReturnEmptyList() {
+        var notExistedName = anyString();
+        var anyPageable = any(Pageable.class);
+
+        Page<Event> actualEvents = eventRepository.findAllByOwnerUsername(notExistedName, anyPageable);
+
+        assertThat(actualEvents).isEmpty();
+    }
+}
---
+++ src/test/resources/sql/test-data-insert.sql
@@ -0,0 +1,107 @@
+-- Test data for "country" table
+INSERT INTO country (id, name, description)
+VALUES (1, 'USA', 'United States of America'),
+       (2, 'UK', 'United Kingdom'),
+       (3, 'FR', 'France');
+
+-- Test data for "city" table
+INSERT INTO city (id, name, description)
+VALUES (1, 'New York', 'The largest city in the United States'),
+       (2, 'London', 'Capital city of England'),
+       (3, 'Paris', 'Capital city of France');
+
+-- Test data for "street" table
+INSERT INTO street (id, type, name)
+VALUES (1, 'Main', 'Broadway'),
+       (2, 'Avenue', 'Oxford Street'),
+       (3, 'Boulevard', 'Champs-Élysées');
+
+-- Test data for "address" table
+INSERT INTO address (id, building_number, room_number, country_id, city_id, street_id)
+SELECT generate_series,
+       generate_series * 100,
+       CASE
+           WHEN generate_series % 3 = 0
+               THEN generate_series * 10
+           END,
+       generate_series % 3 + 1,
+       generate_series % 3 + 1,
+       generate_series % 3 + 1
+FROM generate_series(1, 30);
+
+-- Test data for "users" table
+INSERT INTO users (id, username, email, password, status, network_status, is_verified, role)
+SELECT generate_series,
+       'user' || generate_series,
+       'user' || generate_series || '@example.com',
+       'password' || generate_series,
+       'active',
+       CASE
+           WHEN generate_series % 2 = 0
+               THEN 'online'
+           ELSE 'offline' END,
+       generate_series % 2 = 0,
+       CASE
+           WHEN generate_series % 3 = 0
+               THEN 'admin'
+           ELSE 'user' END
+FROM generate_series(1, 30);
+
+-- Test data for "event" table
+INSERT INTO event (id, name, event_time, description, owner_id, address_id)
+SELECT generate_series,
+       'Event' || generate_series,
+       current_timestamp + INTERVAL '1' DAY * (generate_series % 3 + 1),
+       'Description of Event' || generate_series,
+       generate_series % 3 + 1,
+       generate_series % 3 + 1
+FROM generate_series(1, 90);
+
+-- Test data for "place" table
+INSERT INTO place (id, name, category, description, address_id)
+SELECT generate_series,
+       'Place' || generate_series,
+       CASE
+           WHEN generate_series % 3 = 0
+               THEN 'Park'
+           WHEN generate_series % 3 = 1
+               THEN 'Palace'
+           ELSE 'Landmark' END,
+       'Description of Place' || generate_series,
+       generate_series
+FROM generate_series(1, 30);
+
+-- Test data for "user_event" table
+INSERT INTO user_event (user_id, event_id)
+SELECT generate_series,
+       floor(random() * 30) + 1
+FROM generate_series(1, 30);
+
+-- Test data for "chat" table
+INSERT INTO chat (id, status, owner_id)
+SELECT generate_series,
+       CASE
+           WHEN generate_series % 2 = 0
+               THEN 'active'
+           ELSE 'inactive' END,
+       generate_series % 30 + 1
+FROM generate_series(1, 30);
+
+-- Test data for "user_chat" table
+INSERT INTO user_chat (user_id, chat_id)
+SELECT generate_series,
+       floor(random() * 30) + 1
+FROM generate_series(1, 30);
+
+-- Test data for "chat_message" table
+INSERT INTO chat_message (id, content, status, send_time, chat_id, user_id)
+SELECT generate_series,
+       'Message ' || generate_series,
+       CASE
+           WHEN generate_series % 2 = 0
+               THEN 'sent'
+           ELSE 'received' END,
+       now() + INTERVAL '1' HOUR * generate_series,
+       generate_series % 30 + 1,
+       generate_series % 30 + 1
+FROM generate_series(1, 30);
